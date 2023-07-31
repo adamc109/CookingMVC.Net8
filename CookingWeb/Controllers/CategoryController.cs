1ccc43e26b274911605d247371ddbf6df4118711
Adam <adam1092002@icloud.com>
2023-07-31 22:03:52 +0100

    Added Database & CRUD operations

---
+++ CookingWeb/Controllers/CategoryController.cs
@@ -0,0 +1,103 @@
+﻿using CookingWeb.Data;
+using CookingWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookingWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //connect to data from category using dbcontext
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot be the same as Name");
+            }
+            if (obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is an invalid value");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created sucessfully";
+                return RedirectToAction("Index", "Category");
+            }
+            else return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+
+            //Category? categoryFromDb = _db.Categories.Find(id);
+            Category? categoryFromDb = _db.Categories.FirstOrDefault(u => u.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                TempData["success"] = "Category Updated Sucessfully";
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+
+            Category? categoryFromDb = _db.Categories.FirstOrDefault(u => u.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted Sucessfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CookingWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using CookingWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CookingWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+
+        }
+        
+        //creates table
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Breakfast", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Lunch", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Dinner", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Desert", DisplayOrder = 4 },
+                new Category { Id = 5, Name = "Snack", DisplayOrder = 5 }
+                );
+        }
+
+    }
+}
