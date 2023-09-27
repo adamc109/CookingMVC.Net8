fad6bc57e05fad5539e998eeecdf0c4424c6e1a8
Adam <adam1092002@icloud.com>
2023-09-27 21:22:06 +0100

    Add Cart View

---
+++ CookingWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookingWeb.Areas.Customer.Controllers
+{
+    public class CartController : Controller
+    {
+        [Area("Customer")]
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
