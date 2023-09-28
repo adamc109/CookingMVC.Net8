17f2167ad483942d90fd5f10156a34b252a96f7b
Adam <adam1092002@icloud.com>
2023-09-28 21:14:59 +0100

    Get Shopping Cart

--- CookingWeb/Areas/Customer/Controllers/CartController.cs
+++ CookingWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,15 +1,33 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Cooking.DataAccess.Repository.IRepository;
+using Cooking.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace CookingWeb.Areas.Customer.Controllers
 {
+    [Area("Customer")]
+    [Authorize]
     public class CartController : Controller
     {
-        [Area("Customer")]
-        [Authorize]
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                includeProperties: "Recipie")
+            };
+
+            return View(ShoppingCartVM);
         }
     }
 }
