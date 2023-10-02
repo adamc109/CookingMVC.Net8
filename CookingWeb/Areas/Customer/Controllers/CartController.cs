2b7629028d0f9f6e2b8e47ddcdfe70ed9424fac1
Adam <adam1092002@icloud.com>
2023-10-02 21:51:03 +0100

    Order Summary UI

--- CookingWeb/Areas/Customer/Controllers/CartController.cs
+++ CookingWeb/Areas/Customer/Controllers/CartController.cs
@@ -28,14 +28,56 @@ namespace CookingWeb.Areas.Customer.Controllers
                 includeProperties: "Recipie")
             };
 
-            foreach( var cart in ShoppingCartVM.ShoppingCartList )
+            foreach (var cart in ShoppingCartVM.ShoppingCartList)
             {
                 cart.Price = GetPriceBasedOnQuantity(cart);
                 ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
             }
             return View(ShoppingCartVM);
-          
+
         }
+
+        public IActionResult Summary() {
+            return View();
+        }
+
+        public IActionResult Plus(int cartid)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartid);
+            cartFromDb.Count += 1;
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cartid)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartid);
+            if (cartFromDb.Count <= 1)
+            {
+                //remove from cart
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            }
+            else
+            {
+                cartFromDb.Count -= 1;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartid)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartid);
+
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
 
         private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
         {
@@ -54,7 +96,7 @@ namespace CookingWeb.Areas.Customer.Controllers
                 {
                     return shoppingCart.Recipie.Price100;
                 }
-            }        
+            }
         }
     }
 }
