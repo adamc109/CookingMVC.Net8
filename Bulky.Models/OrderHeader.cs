f6da79225c9c84bc767c8be8c4b4bc973f4aafe0
Adam <adam1092002@icloud.com>
2023-10-03 22:04:14 +0100

    Orderdetails & OrderHeader Models

---
+++ Bulky.Models/OrderHeader.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cooking.Models
+{
+    public class OrderHeader
+    {
+        public int Id { get; set; }
+        public string ApplicationUserID { get; set; }
+        
+        [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
