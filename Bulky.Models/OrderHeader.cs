7d124d933f6e0595fdab361dd7dbf48c6d063572
Adam <adam1092002@icloud.com>
2023-10-04 21:57:09 +0100

    OrderDetails

--- Bulky.Models/OrderHeader.cs
+++ Bulky.Models/OrderHeader.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -16,5 +17,35 @@ namespace Cooking.Models
         [ForeignKey("ApplicationUserId")]
         [ValidateNever]
         public ApplicationUser ApplicationUser { get; set; }
+
+        public DateTime OrderDate { get; set; }
+        public DateTime ShippingDate { get; set; }
+        public double OrderTotal { get; set; }
+
+        public string? OrderStatus { get; set; }
+        public string? PaymentStatus { get; set;}
+        public string? TrackingNumber { get; set; }
+        public string? Carrier { get; set; }
+
+        public DateTime PaymentDate { get; set; }
+        public DateOnly PaymentDueDate { get; set; }
+
+        public string? PaymentIntentId { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string StreetAddress { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string PostalCode { get; set; }
+        [Required]
+        public string Name { get; set; }
+
     }
+
+        
 }
